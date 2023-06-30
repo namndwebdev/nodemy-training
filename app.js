523b95f7128e8513d1d0ff5df1ec904efd66dd0b
devtruongson <truongsonpt.80@gmail.com>
2023-06-30 15:46:26 +0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+    // Gửi tệp HTML theo đường dẫn tương đối
+    res.sendFile(path.join(__dirname, 'public', 'training', 'index.html'));
+});
+
+app.get('/css-grid-attack', (req, res) => {
+    // Gửi tệp HTML theo đường dẫn tương đối
+    res.sendFile(path.join(__dirname, 'public', 'css-grid-attack', 'index.html'));
+});
+
+app.get('/css-selectors-cheatsheet', (req, res) => {
+    // Gửi tệp HTML theo đường dẫn tương đối
+    res.sendFile(path.join(__dirname, 'public', 'css-selectors-cheatsheet', 'index.html'));
+});
+
+app.get('/Dinner', (req, res) => {
+    // Gửi tệp HTML theo đường dẫn tương đối
+    res.sendFile(path.join(__dirname, 'public', 'Dinner', 'index.html'));
+});
+
+app.get('/flexboxfroggy', (req, res) => {
+    // Gửi tệp HTML theo đường dẫn tương đối
+    res.sendFile(path.join(__dirname, 'public', 'flexboxfroggy', 'index.html'));
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
---
+++ public/css-grid-attack/js/d545a1a03d469b3615d6c945e3fc913184e0141d_CSS.6164c81b6ed04bb13dbd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[11],[]]);
+//# sourceMappingURL=d545a1a03d469b3615d6c945e3fc913184e0141d_CSS.6164c81b6ed04bb13dbd.js.map
